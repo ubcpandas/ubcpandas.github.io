2c09a611009fdb38e46268abf57515c72ed93d70
Achinth Bharadwaj <achinth.bharadwaj@gmail.com>
2021-01-07 15:43:21 -0800

    fix with new doc v2 template

---
+++ pandas/docusaurus.config.js
@@ -0,0 +1,105 @@
+module.exports = {
+  title: 'UBC Pandas',
+  tagline: "UBC's new student run data and machine learning design team.",
+  url: 'https://friendly-goldberg-4f6d6b.netlify.app',
+  baseUrl: '/',
+  onBrokenLinks: 'ignore',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'UBC Pandas',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Introduction',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/ubcpandas/',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/ubcpandas/',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} UBC Pandas. Built with Docusaurus. Made with 💖 by the chancellors of UBC pandas.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ pandas/src/pages/index.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+
+const features = [
+  {
+    title: 'Who?',
+    imageUrl: 'img/undraw_docusaurus_mountain.svg',
+    description: (
+      <>
+        Achinth, Adel, Ahnaf, Aishwarya and Vanessa: a bunch of UBC students who wanted to fill this niche with 💖 and 🧠.
+      </>
+    ),
+  },
+  {
+    title: 'What?',
+    imageUrl: 'img/undraw_docusaurus_tree.svg',
+    description: (
+      <>
+        We're a design org, club and an advocacy group and a bunch of chums who really love to have fun with DS and ML.
+      </>
+    ),
+  },
+  {
+    title: 'When?',
+    imageUrl: 'img/undraw_docusaurus_react.svg',
+    description: (
+      <>
+        Come Sept. 2020, we're gonna meet in person on campus. Until then, check out our socials for more Zoom events 🎥 !
+      </>
+    ),
+  },
+  {
+    title: 'Where?',
+    imageUrl: 'img/undraw_docusaurus_react.svg',
+    description: (
+      <>
+       Your computers. Kaggle. Google Colab. Medium. Python venvs. R-Studio. Your 🧠. Probably somewhere on the UBC Campus.
+      </>
+    ),
+  },
+  {
+    title: 'How?',
+    imageUrl: 'img/undraw_docusaurus_react.svg',
+    description: (
+      <>
+        Stay tuned over the next few months to find out more as we substantiate ourselves as a real club and design organization.
+      </>
+    ),
+  },
+  {
+    title: 'Why?',
+    imageUrl: 'img/undraw_docusaurus_react.svg',
+    description: (
+      <>
+       There wasn't a student org which fostered the growth of data scientists and ML engineers. We wanna change that.
+      </>
+    ),
+  },
+];
+
+function Feature({imageUrl, title, description}) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title={`${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className={clsx(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl('docs/')}>
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
